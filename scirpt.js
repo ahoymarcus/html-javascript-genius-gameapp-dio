2190fb878e2d91c2cffee7b140d3067ca36b32d2
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-09-27 21:35:38 -0300

    Inserir as funcoes javascript para criar uma sequencia de botoes e para estilizar os botoes quando selecionados. Inserir um novo seletor de classe css para botoes selected

--- scirpt.js
+++ scirpt.js
@@ -15,6 +15,34 @@ const yellow = document.querySelector('.yellow');
 
 
 
+// Criar ordem aleatória de cores
+let shuffleOrder = () => {
+	let colorOrder = Math.floor(Math.random() * 4);
+	
+	order[order.length] = colorOrder;
+	clickedOrder = [];
+	
+	for (let i in order) {
+		let elementColor = createColorElement(order[i]);
+		
+		lightColor(elementColor, Number(i) + 1);
+	}
+};
+
+
+// Acender a próxima cor
+let lightColor = (element, number) => {
+	number = number * 500;
+	
+	setTimeout(() => {
+		element.classList.add('selected');
+	}, number - 250);
+	
+	setTimeout(() => {
+		element.classList.remove('selected');
+	});
+};
+
 
 
 
